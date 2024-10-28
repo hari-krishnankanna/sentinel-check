08cc60f6d0bdad99244327cd04aeff401f31a1a2
hari-krishnankanna <harikrishnan031997@gmail.com>
2024-10-28 14:21:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   name                = "example-vm"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  size                = "Standard_DS1_v2"
+  size                = "Standard_D2_v3"
 
   admin_username = "hari"  # Use your username
 
