c20e4ba516d9dae49804ae6a5f474951742ea34d
hari-krishnankanna <harikrishnan031997@gmail.com>
2024-10-29 12:10:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -63,8 +63,8 @@ resource "azurerm_network_security_rule" "allow_all" {
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
-  source_port_range           = "22"
-  destination_port_range      = "22"
+  source_port_range           = "21"
+  destination_port_range      = "21"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   network_security_group_name = azurerm_network_security_group.example.name
